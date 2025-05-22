afd22ce076c05ab29bf45a3c04bed51767d72a00
kauanatomb <kauanatomb@gmail.com>
2025-05-22 09:40:07 +0200

    added return 0 for mains

--- ExamRank2/Level1/first_word.c
+++ ExamRank2/Level1/first_word.c
@@ -47,4 +47,5 @@ int main(int argc, char *argv[])
         argv[1]++;
     }
     write(1, "\n", 1);
+    return (0);
 }
--- ExamRank2/Level1/fizz_buzz.c
+++ ExamRank2/Level1/fizz_buzz.c
@@ -60,4 +60,5 @@ int main(void)
         i++;
         write (1, "\n", 1);
     }
+    return (0);
 }
