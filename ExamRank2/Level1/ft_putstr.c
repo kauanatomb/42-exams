d17a2d7e8b93743bc2dba2bc8755d93e74e58a22
kauanatomb <kauanatomb@gmail.com>
2025-05-22 09:40:27 +0200

    added put str

---
+++ ExamRank2/Level1/ft_putstr.c
@@ -0,0 +1,28 @@
+/*
+Write a function that displays a string on the standard output.
+
+The pointer passed to the function contains the address of the string's first
+character.
+
+Your function must be declared as follows:
+
+void	ft_putstr(char *str);
+*/
+
+#include <unistd.h>
+void    ft_putstr(char *str)
+{
+    while (*str)
+    {
+        write (1, str, 1);
+        str++;
+    }
+}
+
+/*
+int main(void)
+{
+    ft_putstr("Hello word!");
+    return (0);
+}
+*/
