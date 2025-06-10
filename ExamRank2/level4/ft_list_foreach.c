fcb6c241dcdad87466d1fa6186a7a7225885006b
kauanatomb <kauanatomb@gmail.com>
2025-06-10 17:26:10 +0200

    added new exercise

---
+++ ExamRank2/level4/ft_list_foreach.c
@@ -0,0 +1,41 @@
+/*
+Write a function that takes a list and a function pointer, and applies this
+function to each element of the list.
+
+It must be declared as follows:
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *));
+
+The function pointed to by f will be used as follows:
+
+(*f)(list_ptr->data);
+
+You must use the following structure, and turn it in as a file called
+ft_list.h:
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+*/
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    t_list  *tmp_lst;
+
+    if (!begin_list || !f)
+		return ;
+    tmp_lst = begin_list;
+    while (tmp_lst)
+    {
+        (*f)tmp_lst->data;
+        tmp_lst = tmp_lst->next;
+    }
+}
