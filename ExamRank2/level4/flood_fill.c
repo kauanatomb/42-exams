a5ed22f159fdf3eafd366bc15d576095ff967893
kauanatomb <kauanatomb@gmail.com>
2025-06-12 14:30:47 +0200

    improved and test flood fill

--- ExamRank2/level4/flood_fill.c
+++ ExamRank2/level4/flood_fill.c
@@ -87,7 +87,6 @@ typedef struct  s_point
     int y;
 }               t_point;
 
-#include "flood_fill.h"
 
 void fill(char **tab, t_point size, t_point p, char target)
 {
@@ -96,15 +95,10 @@ void fill(char **tab, t_point size, t_point p, char target)
     if (tab[p.y][p.x] != target)
         return;
     tab[p.y][p.x] = 'F';
-    t_point next;
-    next = (t_point){p.x + 1, p.y};
-    fill(tab, size, next, target);
-    next = (t_point){p.x - 1, p.y};
-    fill(tab, size, next, target);
-    next = (t_point){p.x, p.y + 1};
-    fill(tab, size, next, target);
-    next = (t_point){p.x, p.y - 1};
-    fill(tab, size, next, target);
+    fill(tab, size, (t_point){p.x + 1, p.y}, target);
+    fill(tab, size, (t_point){p.x - 1, p.y}, target);
+    fill(tab, size, (t_point){p.x, p.y + 1}, target);
+    fill(tab, size, (t_point){p.x, p.y - 1}, target);
 }
 
 void flood_fill(char **tab, t_point size, t_point begin)
@@ -113,3 +107,45 @@ void flood_fill(char **tab, t_point size, t_point begin)
     if (target != 'F')
         fill(tab, size, begin, target);
 }
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+char** make_area(char** zone, t_point size)
+{
+	char** new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+	return new;
+}
+
+int main(void)
+{
+	t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+
+	char** area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point begin = {7, 4}; // ponto inicial
+	flood_fill(area, size, begin);
+
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+
+	return 0;
+}
+*/
