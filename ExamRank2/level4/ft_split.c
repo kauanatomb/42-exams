d223405606ef6ac048e19256b4b6138e07a7811e
kauanatomb <kauanatomb@gmail.com>
2025-06-12 16:50:12 +0200

    done split function

--- ExamRank2/level4/ft_split.c
+++ ExamRank2/level4/ft_split.c
@@ -9,7 +9,8 @@ Your function must be declared as follows:
 
 char    **ft_split(char *str);
 */
-
+#include <stdlib.h>
+#include <stdio.h>
 int	count_words(char *str)
 {
 	int	count;
@@ -17,19 +18,19 @@ int	count_words(char *str)
 	count = 0;
 	while (*str)
 	{
-		while (*str == c)
+		while ((*str >= 8 && *str <= 13) || *str == ' ')
 			str++;
 		if (*str)
 		{
 			count++;
-			while (*str && *str != c)
+			while (*str && !((*str >= 8 && *str <= 13) || *str == ' '))
 				str++;
 		}
 	}
 	return (count);
 }
 
-void    **ft_free(char **strs, int size)
+void    *ft_free(char **strs, int size)
 {
     int i = 0;
 
@@ -39,11 +40,59 @@ void    **ft_free(char **strs, int size)
     return (NULL);
 }
 
+char	*fill_word(char *start, int size)
+{
+	int i = 0;
+	char *str = malloc(size + 1);
+	if (!str)
+		return (NULL);
+	while (i < size)
+	{
+		str[i] = start[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 char    **ft_split(char *str)
 {
+	char *start;
+	int		i = 0;
     if (!str)
         return (NULL);
-    char    **result = malloc((count_words(str) + 1) * sizeof(char *));
+    char **result;
+	result = malloc((count_words(str) + 1) * sizeof(char *));
     if (!result)
         return (NULL);
+	
+    while (*str)
+    {
+	while ((*str >= 8 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str)
+	{
+		start = str;
+		while (*str && !((*str >= 8 && *str <= 13) || *str == ' '))
+			str++;
+		result[i] = fill_word(start, str - start);
+		if (!result[i])
+			ft_free(result, i);
+		i++;
+	}
+    }
+	result[i] = NULL;
+    return (result);
+}
+
+int main()
+{
+	int i = 0;
+	char **result = ft_split(" hello	world ");
+	if (!result)
+		return (0);
+	while (result[i])
+		printf("%s\n", result[i++]);
+	ft_free(result, i);
+	return (0);
 }
