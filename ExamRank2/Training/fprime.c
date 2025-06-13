f7e2f905e18e35d95a49ec2e24c930a2bd4d7a21
kauanatomb <kauanatomb@gmail.com>
2025-06-13 18:06:03 +0200

    added fprime to tranning

---
+++ ExamRank2/Training/fprime.c
@@ -0,0 +1,78 @@
+/*
+Write a program that takes a positive int and displays its prime factors on the
+standard output, followed by a newline.
+
+Factors must be displayed in ascending order and separated by '*', so that
+the expression in the output gives the right result.
+
+If the number of parameters is not 1, simply display a newline.
+
+The input, when there is one, will be valid.
+
+Examples:
+
+$> ./fprime 225225 | cat -e
+3*3*5*5*7*11*13$
+$> ./fprime 8333325 | cat -e
+3*3*5*5*7*11*13*37$
+$> ./fprime 9539 | cat -e
+9539$
+$> ./fprime 804577 | cat -e
+804577$
+$> ./fprime 42 | cat -e
+2*3*7$
+$> ./fprime 1 | cat -e
+1$
+$> ./fprime | cat -e
+$
+$> ./fprime 42 21 | cat -e
+$
+*/
+#include <unistd.h>
+int ft_atoi(char *str)
+{
+    int result = 0;
+    while (*str)
+    {
+        result *= 10;
+        result = result + *str - '0';
+        str++;
+    }
+    return (result);
+}
+
+void    ft_putnbr(int n)
+{
+    if (n > 9)
+        ft_putnbr(n / 10);
+    write(1, &"0123456789"[n % 10], 1);
+}
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        int nbr = ft_atoi(argv[1]);
+        if (nbr == 1)
+            write(1, &"1", 1);
+        int i = 2;
+        while (i < nbr)
+        {
+            if (nbr % i == 0)
+            {
+                ft_putnbr(i);
+                write(1, "*", 1);
+                nbr /= i;
+                i = 1;
+            }    
+            i++;
+        }
+        if (nbr == i && nbr % i == 0)
+        {
+            ft_putnbr(i);
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
