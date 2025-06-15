b966724e366ac7abbbf8bcba7c5b79283165a720
Kauana Tombolato <kauanatomb@gmail.com>
2025-06-15 16:15:44 +0200

    done

--- ExamRank2/Training/ft_list_size.c
+++ ExamRank2/Training/ft_list_size.c
@@ -22,7 +22,7 @@ typedef struct    s_list
     void          *data;
 }                 t_list;
 
-int ft_list_size(t_list *begin_list)
+int	ft_list_size(t_list *begin_list)
 {
     int count = 0;
     while (begin_list)
@@ -32,12 +32,11 @@ int ft_list_size(t_list *begin_list)
     }
     return (count);
 }
+
 #include <stdio.h>
 int main()
 {
-    t_list node3 = {NULL, "Node3"};
-    t_list node2 = {&node3, "Node2"};
-    t_list node1 = {&node2, "Node1"};
-    printf("%d", ft_list_size(&node1));
-    return (0);
+    t_list node3 = {NULL, "Node"};
+    t_list node1 = {&node3, "Node2"};
+    return (ft_list_size(&node1));
 }
