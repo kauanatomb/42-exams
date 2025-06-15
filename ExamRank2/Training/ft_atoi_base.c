582266a8ee6a2c51dd05ba4231a8ddc695dc7f21
Kauana Tombolato <kauanatomb@gmail.com>
2025-06-15 14:28:16 +0200

    done for trainning

--- ExamRank2/Training/ft_atoi_base.c
+++ ExamRank2/Training/ft_atoi_base.c
@@ -21,7 +21,7 @@ int	ft_atoi_base(const char *str, int str_base)
     int sign = 1;
     int result = 0;
 
-    if (!str || str_base < 2 || str_base > 16)
+    if (!str || str_base < 2)
         return (0);
     while ((*str >= 8 && *str <= 13) || *str == ' ')
         str++;
@@ -34,30 +34,27 @@ int	ft_atoi_base(const char *str, int str_base)
     while (*str)
     {
         char c = *str;
-        if (c >= 'A' && c <= 'F')
-            c += 32;
         int val = 0;
-        if (c >= 'a' && c <= 'f')
+        if (c >= 'A' && c <= 'Z')
+            c += 32;
+        if (c >= 'a' && c <= 'z')
             val = 10 + (c - 'a');
-        else if (c >= '0' && c <= '9')
+        if (c >= '0' && c <= '9')
             val = c - '0';
-        else
-            break ;
         if (val >= str_base)
-            break ;
-        result *= str_base;
-        result = result + val;
+            return (0);
+        result = result * str_base + val;
         str++;
     }
     return (sign * result);
 }
-
 #include <stdio.h>
 int main()
 {
-    printf("%d\n", ft_atoi_base("    -1a", 16));
+    printf("%d", ft_atoi_base("     2A", 16));
+    printf("%d\n", ft_atoi_base("   -1a", 16));
     printf("%d\n", ft_atoi_base("1011", 2));
-    printf("%d\n", ft_atoi_base("2A", 16));
+    printf("%d\n", ft_atoi_base("77", 8));
     printf("%d\n", ft_atoi_base("123", 4));
     printf("%d\n", ft_atoi_base("   +FF", 16));
     printf("%d\n", ft_atoi_base("G1", 16));
