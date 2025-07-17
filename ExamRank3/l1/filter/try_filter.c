873566c0bca68d6d07c2650a140d23c28292b65a
kauanatomb <kauanatomb@gmail.com>
2025-07-17 15:33:47 +0200

    practicing filter

--- ExamRank3/l1/filter/try_filter.c
+++ ExamRank3/l1/filter/try_filter.c
@@ -1,61 +1,63 @@
-# define BUF_SIZE 4
-
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int str_match(char *s1, char *s2)
+#ifndef BUF_SIZE
+# define BUF_SIZE 4
+#endif
+
+int str_match(char *s, char *s2)
 {
-    while (*s1 && *s2 && *s1 == *s2)
+    while (*s && *s2 && *s == *s2)
     {
+        s++;
         s2++;
-        s1++;
     }
     if (*s2 == '\0')
-        return (1);
-    return (0);
+        return 1;
+    return 0;
 }
 
-void    print_stars(int i)
+void    print_starts(int i)
 {
-    while (i--)
+    while(i--)
         write(1, "*", 1);
 }
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-    if (argc != 2 || argv[1][0] == '\0' || BUF_SIZE < 1)
-        return (1);
-    int needle_len = strlen(argv[1]);
+    if (argc != 2)
+        return 1;
+    int len = strlen(argv[1]);
+    int b_read = 0;
     char *leftover = NULL;
-    int rd = 0;
-    char buf[BUF_SIZE];
-    while ((rd= read(0, buf, BUF_SIZE)) > 0)
+    char *buf[BUF_SIZE];
+    while ((b_read = read(0, buf, BUF_SIZE)) > 0)
     {
         int left_len = leftover ? strlen(leftover) : 0;
-        int total = rd + left_len;
+        int total = left_len + b_read;
         char *tmp = malloc(total + 1);
         if (!tmp)
         {
-            perror("Error");
             free(leftover);
-            return (1);
+            perror("Error");
+            return 1;
         }
+        tmp[total] = '\0';
         if (leftover)
         {
             memmove(tmp, leftover, left_len);
             free(leftover);
         }
-        memmove(tmp + left_len, buf, rd);
-        tmp[total] = '\0';
+        memmove(tmp + left_len, buf, b_read);
         int i = 0;
-        while (i < total - needle_len)
+        while (i < total - len)
         {
             if (str_match(&tmp[i], argv[1]))
             {
-                print_stars(needle_len);
-                i += needle_len;
+                print_starts(len);
+                i += len;
             }
             else
             {
@@ -63,27 +65,27 @@ int main(int argc, char *argv[])
                 i++;
             }
         }
-        leftover = malloc(total + 1 - i);
+        leftover = malloc(total - i + 1);
         if (!leftover)
         {
-            perror("Error");
             free(tmp);
-            return (1);
+            perror("Error");
+            return 1;
         }
         memmove(leftover, &tmp[i], total - i);
         leftover[total - i] = '\0';
         free(tmp);
     }
-    if (rd < 0)
+    if (b_read < 0)
     {
         perror("Error");
         free(leftover);
-        return (1);
+        return 1;
     }
     if (leftover)
     {
         write(1, leftover, strlen(leftover));
         free(leftover);
     }
-    return (0);
+    return 0;
 }
