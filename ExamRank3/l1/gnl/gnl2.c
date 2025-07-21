31e2a2c299dbbfa7fa1fcf1cf0a39dc5a726bbec
kauanatomb <kauanatomb@gmail.com>
2025-07-21 12:31:04 +0200

    training

--- ExamRank3/l1/gnl/gnl2.c
+++ ExamRank3/l1/gnl/gnl2.c
@@ -2,37 +2,37 @@
 # define BUF_SIZE 4
 #endif
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 char *ft_strdup(char *s)
 {
     if (!s)
         return NULL;
-    int i = 0;
-    while (s[i])
-        i++;
-    char *str = malloc(i + 1);
-    i = 0;
-    while (s[i])
+    int len = 0;
+    while (s[len])
+        len++;
+    char *str = malloc(len + 1);
+    len = 0;
+    while (s[len])
     {
-        str[i] = s[i];
-        i++;
+        str[len] = s[len];
+        len++;
     }
-    str[i] = '\0';
+    str[len] = '\0';
     return str;
 }
 
 char *gnl(int fd)
 {
-    if (fd < 0 || BUF_SIZE <= 0)
+    if (fd < 0 || BUF_SIZE < 0)
         return NULL;
+    char line[70000];
     static int pos = 0;
     static int read_b = 0;
     static char buf[BUF_SIZE];
-    char line[70000];
     int i = 0;
     while (1)
     {
@@ -41,7 +41,7 @@ char *gnl(int fd)
             read_b = read(fd, buf, BUF_SIZE);
             pos = 0;
             if (read_b <= 0)
-                break;
+                break ;
         }
         line[i] = buf[pos++];
         if (line[i++] == '\n')
@@ -50,7 +50,7 @@ char *gnl(int fd)
     if (i == 0)
         return NULL;
     line[i] = '\0';
-    return (ft_strdup(line));
+    return ft_strdup(line);
 }
 
 int main()
