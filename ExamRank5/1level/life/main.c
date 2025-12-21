cf3e0f8601c22081bf14e1a67074d423228730bb
Kauana Tombolato <kauanatomb@gmail.com>
2025-12-21 19:09:51 +0100

    improving to avoid leaks and semantic

--- ExamRank5/1level/life/main.c
+++ ExamRank5/1level/life/main.c
@@ -3,33 +3,32 @@
 #include <stdio.h>
 
 void fill_board(char *board, int w, int h) {
-    int x = 0, y = 0;
+    int j = 0, i = 0;
     int draw = 0;
     char c;
 
     while(read(0, &c, 1) == 1) {
-        if (c == 'w' && y > 0) y--;
-        else if (c == 's' && y < h - 1) y++;
-        else if (c == 'a' && x > 0) x--;
-        else if (c == 'd' && x < w - 1) x++;
+        if (c == 'w' && i > 0) i--;
+        else if (c == 's' && i < h - 1) i++;
+        else if (c == 'a' && j > 0) j--;
+        else if (c == 'd' && j < w - 1) j++;
         else if (c == 'x') draw = !draw;
 
         if (draw)
-            board[y * w + x] = 'O';
+            board[i * w + j] = 'O';
     }
 }
 
-int count_neighbors(char *b, int w, int h, int x, int y) {
+int count_neighbors(char *b, int w, int h, int j, int i) {
     int c = 0;
-    for(int dy = -1; dy <= 1; dy++) {
-        for(int dx = -1; dx <= 1; dx++) {
-            if (dx == 0 && dy == 0)
+    for(int di = -1; di <= 1; di++) {
+        for(int dj = -1; dj <= 1; dj++) {
+            if (dj == 0 && di == 0)
                 continue;
-            int nx = x + dx;
-            int ny = y + dy;
-
-            if (nx >= 0 && ny >= 0 && nx < w && ny < h)
-                if (b[ny * w + nx] == 'O')
+            int ni = i + di;
+            int nj = j + dj;
+            if (nj >= 0 && ni >= 0 && nj < w && ni < h)
+                if (b[ni * w + nj] == 'O')
                     c++;
         }
     }
@@ -37,24 +36,24 @@ int count_neighbors(char *b, int w, int h, int x, int y) {
 }
 
 void step(char *b, char *n, int w, int h) {
-    for(int y = 0; y < h; y++) {
-        for(int x = 0; x < w; x++) {
-            int alive = (b[y * w + x] == 'O');
-            int nb = count_neighbors(b, w, h, x, y);
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            int alive = (b[i * w + j] == 'O');
+            int nb = count_neighbors(b, w, h, j, i);
             if (alive && (nb == 2 || nb == 3))
-                n[y* w + x] = 'O';
+                n[i * w + j] = 'O';
             else if (!alive && nb == 3)
-                n[y* w + x] = 'O';
+                n[i * w + j] = 'O';
             else
-                n[y * w + x] = ' ';
+                n[i * w + j] = ' ';
         }
     }
 }
 
 void print_board(char *b, int w, int h) {
-    for(int y = 0; y < h; y++) {
-        for(int x = 0; x < w; x++)
-            putchar(b[y * w + x]);
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++)
+            putchar(b[i * w + j]);
         putchar('\n');
     }
 }
@@ -66,11 +65,12 @@ int main(int argc, char **argv) {
     int h = atoi(argv[2]);
     int it = atoi(argv[3]);
 
-    char *board = calloc(w * h, 1);
-    char *next = malloc(w * h);
-
-    if (!board || !next)
+    char *board = calloc(w * h, sizeof(char));
+    if (!board)
         return 1;
+    char *next = calloc(w * h, sizeof(char));
+    if (!next)
+        return free(board), 1;
 
     for(int i = 0; i < w * h; i++)
         board[i] = ' ';
